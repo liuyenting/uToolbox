ebbad16c17f371ed98c6bdea7203e286094859ff
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-07-06 17:22:44 +0800

    "Bump version from 0.6.7 to 0.6.8.dev0"

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     #   bumpversion release
     # to next version
     #   bump patch/minor/major
-    version="0.6.7",
+    version="0.6.8.dev0",
     # one-line description for the summary field
     description="Metapackage for uToolbox, a Python image processing package for LLSM.",
     long_description=long_description,
